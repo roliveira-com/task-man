76ba50cf310c1c4dceea906683b1038ee018aec1
Rodrigo Oliveira <rodrigo.oliveira@trigg.com.br>
2018-05-28 13:39:30 -0300

    isolando criação do webhook

---
+++ api/helpers/webhook-create.js
@@ -0,0 +1,47 @@
+module.exports = {
+
+  friendlyName: 'Create Webhook',
+
+  description: 'Configura o objeto e registra webhooks na base',
+
+  inputs: {
+    webhook: {
+      friendlyName: 'Webhook',
+      description: 'Objeto do webhook',
+      type: 'ref',
+      required: true
+    }
+  },
+
+  fn: async (inputs, exits) => {
+
+    let new_webhook = {
+      targetListModel : inputs.webhook.targetListModel || null,
+      targeCardModel  : inputs.webhook.targeCardModel || null,
+      idModel         : inputs.webhook.modelId,
+      description     : `webhook para o model ${inputs.webhook.modelId}`,
+      active          : inputs.webhook.active,
+      trelloId        : inputs.webhook.id,
+      callbackURL     : inputs.webhook.callbackURL,
+    }
+
+    Webhook.create(new_webhook)
+      .fetch()
+      .then(register => {
+        sails.sockets.blast('webhook', { verb: "created", id: webhook.id, data: webhook });
+        sails.log(`CADASTRO DO WEBHOOK ${webhook.id} FEITO COM SUCESSO`);
+        res.status(200).send({
+          error: false,
+          message: 'Lista adicionada com sucesso'
+        })
+      })
+      .catch(error => {
+        sails.log('ERRO NO CADASTRO DO WEBHOOK NA BASE', error);
+        res.status(500).send({
+          error: error,
+          message: 'O cadastro do webhook foi feito no Trello mas não em nossa base'
+        })
+      })
+  }
+
+}
